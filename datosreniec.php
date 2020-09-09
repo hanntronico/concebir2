f48ada0fdbeade731284e98496439fed2478cf28
hanntronico <pedro.bernal.84@gmail.com>
2020-09-09 16:04:47 -0500

    cambios 2 en datosreniec.php

--- datosreniec.php
+++ datosreniec.php
@@ -1,23 +1,23 @@
 <?php
 
-
+	// phpinfo();
 
 	// $datosreniec = json_decode(file_get_contents('http://104.200.144.70:8085/api/datosreniec/08714804'), true);
 
 // https://104.200.144.70/api/datosreniec/70435464 ------ nueva URL
 
-	$api="https://104.200.144.70/api/datosreniec/".$_POST["dni"];
-	// $api="https://104.200.144.70/api/datosreniec/70435464";
+	// $api="https://104.200.144.70/api/datosreniec/".$_POST["dni"];
+	$api="https://104.200.144.70/api/datosreniec/70435464";
 	$datosreniec = json_decode(file_get_contents($api), true);
 
+	print_r($datosreniec);	
 
-
-	if ($datosreniec['status']=='1') {
-		echo $datosreniec['objModel']['nombres']."-".$datosreniec['objModel']['apellidoPaterno']." ".$datosreniec['objModel']['apellidoMaterno'];
-	}
-	elseif($datosreniec['status']=='0'){
-		echo "NA";		
-	}
+	// if ($datosreniec['status']=='1') {
+	// 	echo $datosreniec['objModel']['nombres']."-".$datosreniec['objModel']['apellidoPaterno']." ".$datosreniec['objModel']['apellidoMaterno'];
+	// }
+	// elseif($datosreniec['status']=='0'){
+	// 	echo "NA";		
+	// }
 
 
 
