14f02438ffa0940fa908401cdce27b3dbf35e884
hanntronico <pedro.bernal.84@gmail.com>
2020-09-09 16:09:48 -0500

    cambios 4 en datosreniec.php

--- datosreniec.php
+++ datosreniec.php
@@ -6,11 +6,13 @@
 
 // https://104.200.144.70/api/datosreniec/70435464 ------ nueva URL
 
-	// $api="https://104.200.144.70/api/datosreniec/".$_POST["dni"];
-	$api="https://104.200.144.70/api/datosreniec/70435464";
-	$datosreniec = json_decode(file_get_contents($api), true);
+	echo $_POST["dni"];
 
-	echo $datosreniec['status'];
+	// $api="https://104.200.144.70/api/datosreniec/".$_POST["dni"];
+	// $api="https://104.200.144.70/api/datosreniec/70435464";
+	// $datosreniec = json_decode(file_get_contents($api), true);
+
+	// echo $datosreniec['status'];
 
 	// if ($datosreniec['status']=='1') {
 	// 	echo $datosreniec['objModel']['nombres']."-".$datosreniec['objModel']['apellidoPaterno']." ".$datosreniec['objModel']['apellidoMaterno'];
