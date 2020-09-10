eba6e9d076de48947933ea5c6bbf56cf7cd8f751
hanntronico <pedro.bernal.84@gmail.com>
2020-09-10 10:25:49 -0500

    cambio datosreniec.php

--- datosreniec.php
+++ datosreniec.php
@@ -1,16 +1,63 @@
 <?php
 
-	// phpinfo();
+//API URL
+$url = 'https://104.200.144.70/api/datosreniec/70435464';
+
+// $url ='http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=demo';
+  $json = file_get_contents($url);
+  $array = json_decode($json,true);
+
+  print_r($array);
+
+// //create a new cURL resource
+// $ch = curl_init($url);
+
+// //setup request to send json via POST
+// $data = array(
+//     'username' => 'codexworld',
+//     'password' => '123456'
+// );
+
+// $payload = json_encode(array("user" => $data));
+
+// //attach encoded JSON string to the POST fields
+// // curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+
+// //set the content type to application/json
+// curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+
+// //return response instead of outputting
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// //execute the POST request
+// $result = curl_exec($ch);
+
+// var_dump($result);
+
+// //close cURL resource
+// curl_close($ch);
+    
+
+
+
+
+
+
+
+
+
+
+
 
 	// $datosreniec = json_decode(file_get_contents('http://104.200.144.70:8085/api/datosreniec/08714804'), true);
 
 // https://104.200.144.70/api/datosreniec/70435464 ------ nueva URL
 
 	// $api="https://104.200.144.70/api/datosreniec/".$_POST["dni"];
-	$api="https://104.200.144.70/api/datosreniec/70435464";
-	$datosreniec = json_decode(file_get_contents($api), true);
+	// $api="http://104.200.144.70/api/datosreniec/70435464";
+	// $datosreniec = json_decode(file_get_contents($api), true);
 
-	var_dump($datosreniec);
+	// var_dump($datosreniec);
 
 	// echo $datosreniec['status'];
 
